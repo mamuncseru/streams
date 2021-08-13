6670c909be11ba78f912d9ab0d652541fdb6b692
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-08-13 23:53:25 +0600

    Authentication with google auth created normaly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
 
 const App = () => {
     return <div>
-        <h1>Header!</h1>
         <BrowserRouter>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
