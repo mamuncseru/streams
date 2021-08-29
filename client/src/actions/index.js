1d5580dc07b5d2734c52a45a225d8600b77c0a8e
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-08-29 08:57:06 +0600

    Modal created

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from "../apis/streams";
+import history from '../history';
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -22,28 +24,32 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("./streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("./streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
+  
 };
 
-export const fetchStream = () => async dispatch => {
+export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
 
     dispatch({ type: FETCH_STREAMS, payload: response.data });
 }
 
-export const fetchStreams = (id) => async dispatch => {
+export const fetchStream = (id) => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch => {
