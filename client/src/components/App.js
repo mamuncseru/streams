811033d364076c61a35bbfdb6b033cccd0ed5274
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-03 00:35:29 +0600

    Stream project completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
-import StreamDelete from './streams/StreamDelete';
-import Header from './Header';
-import history from '../history';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
+import history from "../history";
 
 const App = () => {
-    return <div className="ui container">
-        <Router history={history}>
-            <Header />
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit/:id" exact component={StreamEdit} />
-            <Route path="/streams/delete" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
-        </Router>
-    </div>;
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
